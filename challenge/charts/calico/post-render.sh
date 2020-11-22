5d2528fb2e7b97f94cfec853ad7a05033443304c
Jan-Otto Kröpke <joe@adorsys.de>
2020-11-22 18:58:45 +0100

    Use helmfile

--- challenge/charts/calico/post-render.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cat - > "${DIR}/kustomize/all.yaml"
-kubectl kustomize "${DIR}/kustomize"
-rm "${DIR}/kustomize/all.yaml"
