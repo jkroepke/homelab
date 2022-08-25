afb2d92ee1e3193449688de3620a6cfa37f32b30
Jan-Otto Kröpke <jok@cloudeteer.de>
2022-08-25 09:31:19 +0200

    use legacy names inside subnet

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -4,14 +4,14 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   availability_zone = each.key
 
-  assign_ipv6_address_on_creation = true
+  assign_ipv6_address_on_creation = false
   enable_dns64                    = false
   map_public_ip_on_launch         = false
 
-  enable_resource_name_dns_a_record_on_launch    = true
-  enable_resource_name_dns_aaaa_record_on_launch = true
+  enable_resource_name_dns_a_record_on_launch    = false
+  enable_resource_name_dns_aaaa_record_on_launch = false
 
-  private_dns_hostname_type_on_launch = "resource-name"
+  private_dns_hostname_type_on_launch = "ip-name"
 
   cidr_block      = cidrsubnet(var.private_subnet_cidr, 2, each.value)
   ipv6_cidr_block = cidrsubnet(aws_vpc.this.ipv6_cidr_block, 8, var.private_ipv6_net_id + each.value)
@@ -35,7 +35,7 @@ resource "aws_subnet" "public" {
   enable_resource_name_dns_a_record_on_launch    = true
   enable_resource_name_dns_aaaa_record_on_launch = true
 
-  private_dns_hostname_type_on_launch = "resource-name"
+  private_dns_hostname_type_on_launch = "ip-name"
 
   cidr_block      = cidrsubnet(var.public_subnet_cidr, 2, each.value)
   ipv6_cidr_block = cidrsubnet(aws_vpc.this.ipv6_cidr_block, 8, var.public_ipv6_net_id + each.value)
