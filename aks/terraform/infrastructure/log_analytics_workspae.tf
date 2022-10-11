54556f9cd20a4a995ba4a94bc871478b21be987b
Jan-Otto Kröpke <mail@jkroepke.de>
2022-10-11 22:59:10 +0200

    Add log analytics workspace

---
+++ aks/terraform/infrastructure/log_analytics_workspae.tf
@@ -0,0 +1,7 @@
+resource "azurerm_log_analytics_workspace" "default" {
+  name                = "default"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  sku                 = "Free"
+  retention_in_days   = 3
+}
