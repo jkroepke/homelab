676b0612dbadc29f9b780f834e54d99f0dbf41a2
Jan-Otto Kröpke <mail@jkroepke.de>
2022-10-20 17:11:31 +0200

    use azapi

--- aks/terraform/infrastructure/modules/federated-managed-identity/main.tf
+++ aks/terraform/infrastructure/modules/federated-managed-identity/main.tf
@@ -4,65 +4,18 @@ resource "azurerm_user_assigned_identity" "this" {
   location            = var.location
 }
 
-resource "null_resource" "federated-credentials" {
-  triggers = {
-    id                  = azurerm_user_assigned_identity.this.id
-    name                = var.name
-    subject             = var.subject
-    oidc_issuer_url     = var.oidc_issuer_url
-    resource_group_name = var.resource_group_name
-  }
-
-  # https://learn.microsoft.com/en-us/azure/aks/workload-identity-deploy-cluster
-  # https://github.com/hashicorp/terraform-provider-azurerm/issues/18617
-  provisioner "local-exec" {
-    command = join(" ", [
-      "az", "identity", "federated-credential", "create",
-      "--resource-group ${self.triggers.resource_group_name}",
-      "--identity-name ${self.triggers.name}",
-      "--name ${replace(self.triggers.subject, ":", "-")}",
-      "--issuer ${self.triggers.oidc_issuer_url}",
-      "--subject ${var.subject}",
-    ])
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = join(" ", [
-      "az", "identity", "federated-credential", "delete",
-      "--resource-group ${self.triggers.resource_group_name}",
-      "--identity-name ${self.triggers.name}",
-      "--name argocd--argocd-repo-server",
-      "--yes",
-    ])
-  }
-}
-
-/*
-resource "azapi_resource" "federated-credentials" {
-  type      = "Microsoft.ContainerRegistry/registries@2020-11-01-preview"
-  name      = "registry1"
-  parent_id = azurerm_resource_group.example.id
-
-  location = azurerm_resource_group.example.location
-  identity {
-    type         = "SystemAssigned, UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.example.id]
-  }
+# https://github.com/hashicorp/terraform-provider-azurerm/issues/18617
+resource "azapi_resource" "federated_identity_credential" {
+  schema_validation_enabled = true
+  name                      = replace(var.subject, ":", "-")
+  parent_id                 = azurerm_user_assigned_identity.this.id
+  type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
 
   body = jsonencode({
-    sku = {
-      name = "Standard"
-    }
     properties = {
-      adminUserEnabled = true
+      issuer    = var.oidc_issuer_url
+      subject   = var.subject
+      audiences = ["api://AzureADTokenExchange"]
     }
   })
-
-  tags = {
-    "Key" = "Value"
-  }
-
-  response_export_values = ["properties.loginServer", "properties.policies.quarantinePolicy.status"]
 }
-*/
