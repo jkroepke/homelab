84ce0964c80dfabaa63c63abaea89a0bd8087061
Jan-Otto Kröpke <mail@jkroepke.de>
2022-10-28 17:51:15 +0200

    Example for generic output

--- aks/terraform/infrastructure/az_aks.tf
+++ aks/terraform/infrastructure/az_aks.tf
@@ -57,7 +57,7 @@ resource "azurerm_kubernetes_cluster" "jok" {
   }
 
   identity {
-    type = "UserAssigned"
+    type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.aks.id]
   }
 
