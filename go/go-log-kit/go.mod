7706265e87def5e1ec47ecb27be735c6de1df00a
Jan-Otto Kröpke <mail@jkroepke.de>
2023-04-23 13:56:44 +0200

    wip

---
+++ go/go-log-kit/go.mod
@@ -0,0 +1,10 @@
+module github.com/jkroepke/homelab/go/go-log-kit
+
+go 1.20
+
+require (
+	github.com/go-kit/log v0.2.1
+	github.com/prometheus/common v0.42.0
+)
+
+require github.com/go-logfmt/logfmt v0.5.1 // indirect
---
+++ go/go-log-kit/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/common/promlog"
+	stdlog "log"
+)
+
+func main() {
+	promlogConfig := promlog.Config{
+		Level: func() *promlog.AllowedLevel {
+			level := promlog.AllowedLevel{}
+			level.Set("debug")
+			return &level
+		}(),
+		Format: func() *promlog.AllowedFormat {
+			format := promlog.AllowedFormat{}
+			format.Set("logfmt")
+			return &format
+		}(),
+	}
+
+	logger := promlog.New(&promlogConfig)
+	stdlogger := stdlog.New(log.NewStdlibAdapter(log.With(level.Error(logger))), "", stdlog.Lshortfile)
+
+	err := errors.New("write tcp 127.0.0.1:9182->127.0.0.1:60125: wsasend: Eine bestehende Verbindung wurde softwaregesteuert\r\ndurch den Hostcomputer abgebrochen.")
+	stdlogger.Println("error encoding and sending metric family:", err)
+}
