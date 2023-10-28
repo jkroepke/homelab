026e4b3295c7beb16f5209abcc210028f5433ddd
Jan-Otto Kröpke <mail@jkroepke.de>
2023-10-28 19:24:08 +0200

    wip

--- aks/resource-scale.sh
+++ aks/resource-scale.sh
@@ -3,7 +3,7 @@
 for i in $(seq 0 50 800); do
   echo $i
 
-  for j in $(seq "$i" $((i + 50)) ); do
+  for j in $(seq "$i" $((i + 49)) ); do
     az keyvault create --no-wait --only-show-errors --location westus3 --name "kv-scale-$j" --resource-group rg-resource-scale --subscription 1988b893-553c-4652-bd9b-52f089b21ead &
   done
 
@@ -16,7 +16,7 @@ done
 for i in $(seq 800 50 1600); do
   echo $i
 
-  for j in $(seq "$i" $((i + 50)) ); do
+  for j in $(seq "$i" $((i + 49)) ); do
     az keyvault create --no-wait --only-show-errors --location westus3 --name "kv-scale-$j" --resource-group rg-resource-scale-2 --subscription 1988b893-553c-4652-bd9b-52f089b21ead &
   done
 
